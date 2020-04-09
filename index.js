4717f6a48a09d53ac14197d1b03f769bd7b44ca4
Iago Henrique <iago.hcc@gmail.com>
2020-04-08 22:53:36 -0300

    git commit -m "curso node.js"

---
+++ index.js
@@ -0,0 +1,42 @@
+const restify = require('restify');
+
+const server = restify.createServer({
+  name: 'myapp',
+  version: '1.0.0'
+});
+
+const knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host : '127.0.0.1',
+    user : 'root',
+    password : '',
+    database : 'db'
+  }
+});
+
+server.use(restify.plugins.acceptParser(server.acceptable));
+server.use(restify.plugins.queryParser());
+server.use(restify.plugins.bodyParser());
+
+
+
+server.listen(8080, function () {
+  console.log('%s listening at %s', server.name, server.url);
+});
+
+server.get('/', function (req, res, next) {
+ knex('rest').then((dados)=>{
+   res.send(dados);
+
+ }, next);
+});
+
+server.post('/create', function (req, res, next) {
+  knex('rest')
+  .insert(req.body)
+  .then((dados)=>{
+  res.send(dados);
+ 
+  }, next);
+ });
